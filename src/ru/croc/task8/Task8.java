153735f3e3dbbd5a74b7bfe0c4469af053428cb7
Vl-Tershch <ddiavall@yandex.ru>
2022-11-14 20:10:17 +0300

    Fixed remarks in the eighth task

--- src/ru/croc/task8/Task8.java
+++ src/ru/croc/task8/Task8.java
@@ -2,27 +2,27 @@ package ru.croc.task8;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Task8 {
     public static void main(String[] args) {
 
         // Initial variables and default values
-        Map<String, Locale> allLocales = Stream.of(new Object[][] {
-                { "usa", Locale.US },
-                { "france", Locale.FRANCE },
-                {"germany", Locale.GERMANY},
-                {"japan", Locale.JAPAN},
-                {"italy", Locale.ITALY},
-                {"korea", Locale.KOREA},
-                {"china", Locale.CHINA},
-                {"canada", Locale.CANADA},
-                {"uk", Locale.UK}
-        }).collect(Collectors.toMap(data -> (String) data[0], data -> (Locale) data[1]));
+        HashMap<String, Locale> allLocales = new HashMap<>() {
+            {
+                put("usa",Locale.US);
+                put("france",Locale.FRANCE);
+                put("germany",Locale.GERMANY);
+                put("japan",Locale.JAPAN);
+                put("italy",Locale.ITALY);
+                put("korea",Locale.KOREA);
+                put("china",Locale.CHINA);
+                put("canada",Locale.CANADA);
+                put("uk",Locale.UK);
+            }
+        };
         Locale locale = Locale.US;
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
 
@@ -32,18 +32,27 @@ public class Task8 {
         String localeString = scanner.nextLine();
         System.out.print("Enter a double: ");
         String priceString = scanner.nextLine();
-        if (!localeString.isEmpty()) {
-            if (!allLocales.containsKey(localeString.toLowerCase())) {
-                throw new IllegalArgumentException("Incorrect input locale: " + locale);
+        try {
+            if (!localeString.isEmpty()) {
+                if (!allLocales.containsKey(localeString.toLowerCase())) {
+                    throw new IllegalArgumentException();
+                } else {
+                    locale = allLocales.get(localeString.toLowerCase());
+                    numberFormat = NumberFormat.getCurrencyInstance(locale);
+                }
             }
-            locale = allLocales.get(localeString.toLowerCase());
-            numberFormat = NumberFormat.getCurrencyInstance(locale);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Incorrect input locale: " + localeString);
         }
 
         // Output the received value
         try {
             BigDecimal price = new BigDecimal(priceString);
-            System.out.println("Result: " + numberFormat.format(price));
+            if (!allLocales.containsKey(localeString.toLowerCase())) {
+                System.out.println("Price using default value: " + numberFormat.format(price));
+            } else {
+                System.out.println("Result: " + numberFormat.format(price));
+            }
         } catch (NumberFormatException e) {
             System.out.println("Can not convert string: " + priceString + " to double!");
         } catch (IllegalArgumentException e) {
